90e56d24f999ccdc8869200fb1f71a18fd52de65
Kobi <abirkoby@gmail.com>
2018-04-17 06:11:46 +0300

    info now displayed according to Data received

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-require('jquery');
-require('bootstrap');
-require('font-awesome');
-require('handlebars');
+// require('jquery');
+// require('bootstrap');
+// require('font-awesome');
+// require('handlebars');
+
+
+
+var fetch = function () {
+    $.ajax({
+      method: "GET",
+      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+      success: function(data) {
+        debugger;
+        console.log(data);
+        console.log(data.items["0"].volumeInfo.title);
+        console.log(data.items["0"].volumeInfo.authors);
+        console.log(data.items["0"].volumeInfo.description);
+        console.log(data.items["0"].volumeInfo.imageLinks.thumbnail);
+        showBookInfo(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      }
+    }); 
+  };
+
+  $(".search-isbn-button").on("click",fetch);
+
+function showBookInfo(data)
+{
+  $(".search-results").empty();
+  $(".search-results").append("<div class=\"search-results-title\">"+data.items["0"].volumeInfo.title+"</div>",
+                              "<p>"+data.items["0"].volumeInfo.description +"</p>",
+                              "<div class=\"search-results-author\">Written by:   "+data.items["0"].volumeInfo.authors+"</div>",
+                              "<img src=\""+data.items["0"].volumeInfo.imageLinks.thumbnail+"\" >");
+       
+
+
+}
